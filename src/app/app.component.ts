6b362eb1e5b8629c483883a19e09e72f3bb16420
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-10-27 17:49:03 -0500

    Include other functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string = 'ProyectoBase';
-  numero1 = 65;
-  numero2 = 23;
-  suma = 0;
+  title : string = 'ProyectoBase'
+  numero1 = 65
+  numero2 = 23
+  suma = 0
+  multiplicacion = 0
 
   sumar() {
-    this.suma = this.numero1 + this.numero2;
+    this.suma = this.numero1 + this.numero2
+  }
+
+  multiplicar(num1: number, num2: number) {
+    this.multiplicacion = num1 * num2
   }
 }
