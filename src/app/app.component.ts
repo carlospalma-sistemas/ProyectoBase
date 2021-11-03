0f13ccdf3f8d27d906a2bace06d8b54fa44d96c4
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-02 21:48:47 -0500

    Completada suma y multiplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title:string = 'Calculadora'
-  multiplicacion:number = 0
-  valor:number = 0
+    title: string = 'Calculadora'
 
-  multiplicar(num1: any, num2: any) {
-    this.multiplicacion = num1 * num2
-  }
+    numero3: number = 0
+    numero4: number = 0
+    suma: number = 0
+    multiplicacion: number = 0
+    resultados: number[] = []
+
+    sumar(num1: string, num2: string): void {
+        this.suma = Number(num1) + Number(num2)
+        this.resultados.push(this.suma)
+    }
+
+    multiplicar(): void {
+        this.multiplicacion = this.numero3 * this.numero4
+        this.resultados.push(this.multiplicacion)
+    }
 }
 
 
