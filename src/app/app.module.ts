3da138d35112519590532380313a2547e656035e
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-04 05:58:01 -0500

    Modify modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FormPersonaComponent } from './form-persona/form-persona.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormPersonaComponent
   ],
   imports: [
     BrowserModule,
