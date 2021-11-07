08b18b096485f4015bd521668ba71590fcf520fa
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-07 17:40:29 -0500

    otro intento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProyectoBase';
+  title:string = 'ProyectoBase';
+  resultado:number = 0;
+  resultado2:number = 0;
+  v3:number = 0;
+  v4:number = 0;
+
+  sumar(v1:string, v2:string):void {
+    this.resultado = Number(v1)+Number(v2);
+  }
+
+  multiplicar():void {
+    this.resultado2 = this.v3 * this.v4;
+  }
 }
