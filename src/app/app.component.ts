38ed5c95b1282175d1e02cf98d748cae96e72ecb
Carlos Andrés Palma Suárez <34041415+carlospalma-sistemas@users.noreply.github.com>
2021-11-08 07:59:38 -0500

    Se incorpora funcionalidades para calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProyectoBase';
+  title:string = 'Proyecto Base';
+  suma:number = 0;
+  val3:number = 0;
+  val4:number = 0;
+  multiplicacion:number = 0;
+
+  cambiarTitulo(titulo:string):void {
+    this.title = titulo
+  }
+
+  sumar(val1:string, val2:string):void {
+    this.suma = Number(val1) + Number(val2)
+  }
+
+  multiplicar():void {
+    this.multiplicacion = this.val3 * this.val4
+  }
 }
